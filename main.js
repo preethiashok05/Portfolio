851fa012fb00a912a2e36c78a1442265bff602f1
Preethi <76838649+PreethiAshok05@users.noreply.github.com>
2021-10-02 22:19:36 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,85 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close'),
+        navLink = document.querySelectorAll('.nav__link');
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+if(navToggle){
+    navToggle.addEventListener('click' ,() =>{
+        navMenu.classList.add('show-menu');
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click' ,() =>{
+        navMenu.classList.remove('show-menu');
+    })
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+function linkAction(){
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click' , linkAction));
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+    for( i=0 ; i < skillsContent.length ; i++)
+    {
+        skillsContent[i].className = 'skills__content skills__close';
+        console.log(itemClass);
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach( (el) =>{
+    el.addEventListener('click' , toggleSkills )
+})
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollTop(){
+    const scrollTop =  document.getElementById('scroll-top');
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll');
+}
+
+/*==================== DARK LIGHT THEME ====================*/ 
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'fa-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => document.body.classList.contains(iconTheme) ? 'fa-moon' : 'fa-sun';
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'fas fa-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click' , ()=> {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme' , getCurrentTheme())
+    localStorage.setItem('selected-icon' , getCurrentIcon())
+})
